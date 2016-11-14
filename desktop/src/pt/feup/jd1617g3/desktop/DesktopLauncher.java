2b1114db539cd40a6b6ae2923b4cba19f1e7ec3f
Luis <luiseddie.kr@gmail.com>
2016-11-14 11:40:17 +0000

    First commit

---
+++ desktop/src/pt/feup/jd1617g3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pt.feup.jd1617g3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pt.feup.jd1617g3.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
