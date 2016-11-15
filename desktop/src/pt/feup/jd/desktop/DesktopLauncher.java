4fb82952e2e9d117980a6f66384d334a88d28bd9
Luis <luiseddie.kr@gmail.com>
2016-11-15 10:11:17 +0000

    Renamed project

---
+++ desktop/src/pt/feup/jd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pt.feup.jd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pt.feup.jd.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
